7fdb7d7efa32f077787c3e3dd234094af4cd667e
shifa <shifa@Sani>
2026-09-23 21:41:53 +0530

    First commit

--- java_code/src/polymorphism/methodoverloading/MethodOverloadingDemo.java
+++ java_code/src/polymorphism/methodoverloading/MethodOverloadingDemo.java
@@ -3,10 +3,10 @@ package polymorphism.methodoverloading;
 public class MethodOverloadingDemo {
 	public static void main(String[] args) {
 		//MethodOverLoading obj=new MethodOverLoading();
-		System.out.println(MethodOverLoading.addition(10, 20));
-		System.out.println(MethodOverLoading.addition(10, 30.2f));
-		System.out.println(MethodOverLoading.addition(40.8f, 30.7f));
-		System.out.println(MethodOverLoading.addition("Great", "India"));
+		System.out.println(MethodOverloading.addition(10, 20));
+		System.out.println(MethodOverloading.addition(10, 30.2f));
+		System.out.println(MethodOverloading.addition(40.8f, 30.7f));
+		System.out.println(MethodOverloading.addition("Great", "India"));
 
 	}
 
