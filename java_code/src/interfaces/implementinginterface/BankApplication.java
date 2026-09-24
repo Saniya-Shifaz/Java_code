04364e4195fef8e41043439dc0aa55ee8a598965
shifa <shifa@Sani>
2026-09-24 22:08:30 +0530

    First commit

---
+++ java_code/src/interfaces/implementinginterface/BankApplication.java
@@ -0,0 +1,14 @@
+package interfaces.implementinginterface;
+
+public class BankApplication {
+	public static void main(String[] args) {
+		Bank b=new BankImpl();
+		Account a=new Account(101,"David",80000,b);
+		
+		b.deposit(a, 20000);
+		System.out.println(a);
+		b.withdraw(a, 10000);
+		System.out.println(a);
+		b.withdraw(a, 90000);
+	}
+}
