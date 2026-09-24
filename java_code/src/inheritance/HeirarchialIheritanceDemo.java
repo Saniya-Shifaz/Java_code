fa051b7fa4316548960c8656883dcffbae7812f8
shifa <shifa@Sani>
2026-09-24 22:33:54 +0530

    first commit

---
+++ java_code/src/inheritance/HeirarchialIheritanceDemo.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+import java.util.Scanner;
+
+public class HeirarchialIheritanceDemo {
+
+	public static void main(String[] args) {
+		Person p;
+		//Loose coupling
+		//p=new Employee();
+     // System.out.println(p);
+      Address ad=new Address("MG Road","Mysore","Karnataka",570028);
+      System.out.println(ad);
+     p=new Employee("Gowri","Mysore",ad,200,50000,1);
+      System.out.println(p);
+      //Data taken dynamically
+     
+	}
+
+}
---
+++ java_code/src/inheritance/SLInheritanceDemo.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class SLInheritanceDemo {
+	public static void main(String[] args) {
+		Student s1=new Student();
+		Student s2=new Student("Manohar",123456789L,"Mysore",6364520807L,678,"VTU");
+		System.out.println(s2);
+	}
+}
