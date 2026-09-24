777f2ebea9a12e3f7123fa5cad6bb6640b45deb9
shifa <shifa@Sani>
2026-09-24 21:40:49 +0530

    first commit

---
+++ java_code/src/polymorphism/methodoverriding/MethodOverriding.java
@@ -0,0 +1,11 @@
+package polymorphism.methodoverriding;
+
+public class MethodOverriding {
+	public static void main(String[] args) {
+		Parent p=new Parent();
+		p.login("Gayatri", "123");
+        p=new Child();
+        p.login("David", "xyz");
+	}
+
+}
---
+++ java_code/src/polymorphism/methodoverriding/Parent.java
@@ -0,0 +1,14 @@
+package polymorphism.methodoverriding;
+
+public class Parent {
+	protected  boolean login(String uname,String pwd) {
+		boolean status=false;
+		if(uname==uname && pwd==pwd)
+		{
+			status=true;
+			System.out.println("Login successfull");
+		}
+		return status ;
+	}
+
+}
